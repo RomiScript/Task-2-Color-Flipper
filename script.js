553058446dcf9b9771eef06ecf9c25647d9781f7
RomiScript <romina.martinez496@gmail.com>
2025-12-06 17:11:35 -0300

    Final Commit

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 let colorcode = document.querySelector('.colorcode');
+let colorBox = document.querySelector('.colorgenerator');
 let btn = document.querySelector('.btn');
 let body = document.querySelector('.body');
 
+let hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 
-let dec = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-
-btn.addEventListener('click', function(){
-    let code = '#';
-    let newcode = '#';
-
-    for (let i=0; i<6; i++){
-        code += dec[Random()];
+function randomHex() {
+    let code = "#";
+    for (let i = 0; i < 6; i++) {
+        code += hex[Math.floor(Math.random() * hex.length)];
     }
-    for (let i=0; i<6; i++){
-        newcode += dec[Random()];
-    }
-    colorcode.innerHTML = code;
-    body.style.background = 'linear-gradient(to right, '+code+', '+newcode+')';
-})
-
-function Random(){
-    return Math.floor(Math.random() * dec.length);
+    return code;
 }
+
+btn.addEventListener('click', () => {
+    const c1 = randomHex();
+    const c2 = randomHex();
+
+    colorcode.textContent = c1;
+    body.style.background = `linear-gradient(to right, ${c1}, ${c2})`;
+});
+
+/* CLICK-TO-COPY */
+colorBox.addEventListener('click', () => {
+    navigator.clipboard.writeText(colorcode.textContent);
+
+    colorBox.classList.add("copied");
+    setTimeout(() => colorBox.classList.remove("copied"), 700);
+});
